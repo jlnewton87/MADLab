b6f00e1ba5c569a2a7aa2f47fd3c308da83752a8
Ibrahime Balde <coast04@gmail.com>
2012-12-10 00:55:44 -0500

    reverting back to old project to fix error
    reverting back

---
+++ CHSS/gen/ksu/madlab/chssApp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ksu.madlab.chssApp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- PhoneGapTest/gen/com/dracosoftrnd/phonegaptest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.dracosoftrnd.phonegaptest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
